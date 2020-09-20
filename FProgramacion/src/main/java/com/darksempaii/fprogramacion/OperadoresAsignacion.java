17fa24142f1d8c4e23d4942e495cdfe5c6008c83
Darksempaii <cesar_enoc666@hotmail.com>
2020-09-20 11:55:03 -0500

    Practica de asignaciones simples y compuestas (sujeto a combios)

--- FProgramacion/src/main/java/com/darksempaii/fprogramacion/OperadoresAsignacion.java
+++ FProgramacion/src/main/java/com/darksempaii/fprogramacion/OperadoresAsignacion.java
@@ -15,7 +15,22 @@ public class OperadoresAsignacion {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int a = 20, b = 10, c, d, e = 10, f = 4, g = 9;
+        a = (a + 1);
+        b = (b - 1);
+        e = (e * 2);
+        f = (f / 2);
+        System.out.println("1: "+"a,b,e,f "+a+" "+b+" "+e+" "+f);
+        a = a - 1;
+        b = b + 1;
+        e = e / 2;
+        f = f * 2;
+        System.out.println("2: "+"a.b.e.f "+a+" "+b+" "+e+" "+f);
+        a += 1;
+        b -= 1;
+        e *= 2;
+        f /= 2;
+        System.out.println("3: "+"a.b.e.f "+a+" "+b+" "+e+" "+f);
     }
-    
+
 }
