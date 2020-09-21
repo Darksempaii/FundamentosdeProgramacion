1cbbbbc9098b0a24ee3b53e013c74298ca823ea6
Darksempaii <cesar_enoc666@hotmail.com>
2020-09-20 23:31:06 -0500

    Practica Conversor de millas a metros (sujeto a combios)

--- FProgramacion/src/main/java/com/darksempaii/fprogramacion/ConversorMillasMetros.java
+++ FProgramacion/src/main/java/com/darksempaii/fprogramacion/ConversorMillasMetros.java
@@ -1,21 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.darksempaii.fprogramacion;
 
 import java.util.Scanner;
 
-/**
- *
- * @author Darksempaii
- */
+
 public class ConversorMillasMetros {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Ingrese la distancia en millas: ");
@@ -23,7 +13,6 @@ public class ConversorMillasMetros {
         double distancia = Double.parseDouble(dato);
         double metros = distancia * 1609;
         System.out.println("La distancia en Metros es: " +metros);
-//mi = 1609mw;
     }
 
 }
