6d48330fcb02a55bca2773dbfa7e710a2760be6c
Darksempaii <cesar_enoc666@hotmail.com>
2020-09-21 16:51:51 -0500

    Practica de Operadores Logicos1 v1.1

--- FProgramacion/src/main/java/com/darksempaii/fprogramacion/OperadoresLogicos.java
+++ FProgramacion/src/main/java/com/darksempaii/fprogramacion/OperadoresLogicos.java
@@ -5,8 +5,10 @@ package com.darksempaii.fprogramacion;
 public class OperadoresLogicos {
 
     public static void main(String[] args) {
+        //Declaracion de variables y sus valores
         int a = 5;
         int b = 3;
+        //Salida a consola y sus operaciones booleanas
         System.out.println("1.La operacion ( a * b + 2 == 17 ) da a: " + (a * b + 2 == 17));
         System.out.println("2.la operacion (a > b )&&( 2*a <= b) da a: " + ((a > b) && (2 * a <= b)));
         System.out.println("3.la operacion (b++ > 3) || ((a+b) <= 8) && (a>b) da a: " + ((b++ > 3) || ((a + b) <= 8) && (a > b)));
