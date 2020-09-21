ce6a60dcdb174b3b7296e1818a4ed858564d828d
Darksempaii <cesar_enoc666@hotmail.com>
2020-09-21 15:31:40 -0500

    Practica de entrada y salida de datos con JOptionPanel

--- FProgramacion/src/main/java/com/darksempaii/fprogramacion/EntradadeDatos.java
+++ FProgramacion/src/main/java/com/darksempaii/fprogramacion/EntradadeDatos.java
@@ -8,7 +8,7 @@ public class EntradadeDatos {
     public static void main(String[] args) {
         //Entrada grafica
         String nombre_usuario = JOptionPane.showInputDialog("Introduce tu nombre por favor");
-        String edad = JOptionPane.showInputDialog("Introduce tu nombre por favor");
+        String edad = JOptionPane.showInputDialog("Introduce tu edad por favor");
         //conversion a de la variable y su porterior manipulacion
         int edad_usuario = parseInt(edad);
         edad_usuario++;
