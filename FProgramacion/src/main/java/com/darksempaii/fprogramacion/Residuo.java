ac6e4ff86e50203c29e91f7a96023a0f3cd6cda1
Darksempaii <cesar_enoc666@hotmail.com>
2020-09-21 07:52:10 -0500

    Practica deL Residuo dde la divicion (no Fixed)

--- FProgramacion/src/main/java/com/darksempaii/fprogramacion/Residuo.java
+++ FProgramacion/src/main/java/com/darksempaii/fprogramacion/Residuo.java
@@ -5,6 +5,8 @@
  */
 package com.darksempaii.fprogramacion;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Darksempaii
@@ -15,7 +17,21 @@ public class Residuo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Variables
+        float n1, n2;
+        float c, r;
+        //Entrada
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Inserte el dato: ");
+        n1 = sc.nextInt();
+        System.out.print("Ingrese otro dato: ");
+        n2 = sc.nextInt();
+        //Proceso
+        c = n1 / n1;
+        r = n1 % n2;
+        //Salida
+        System.out.println("El conciente es: " + c);
+        System.out.println("El residuo es: " + r);
     }
-    
+
 }
