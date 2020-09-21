9c113ceab6943b89bcfc8e9b4962bbe2b4ac98bb
Darksempaii <cesar_enoc666@hotmail.com>
2020-09-20 23:49:23 -0500

    Practica Conversor de millas a metros (sujeto a combios)

--- FProgramacion/src/main/java/com/darksempaii/fprogramacion/ConversorMillasMetros.java
+++ FProgramacion/src/main/java/com/darksempaii/fprogramacion/ConversorMillasMetros.java
@@ -9,8 +9,7 @@ public class ConversorMillasMetros {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Ingrese la distancia en millas: ");
-        String dato = sc.nextLine();
-        double distancia = Double.parseDouble(dato);
+        double distancia = sc.nextDouble();
         double metros = distancia * 1609;
         System.out.println("La distancia en Metros es: " +metros);
     }
