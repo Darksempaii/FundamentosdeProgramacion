d0c3170d09d377c2e709d4d912547ea11eab3f64
Darksempaii <cesar_enoc666@hotmail.com>
2020-09-21 16:52:05 -0500

    Practica de Operadores Logicos2 v1.1

--- FProgramacion/src/main/java/com/darksempaii/fprogramacion/OperadoresLogicos2.java
+++ FProgramacion/src/main/java/com/darksempaii/fprogramacion/OperadoresLogicos2.java
@@ -4,10 +4,10 @@ package com.darksempaii.fprogramacion;
 public class OperadoresLogicos2 {
 
     public static void main(String[] args) {
-        int a;
-        a = 20;
-        int b;
-        b = 10;
+        //Declaracion de variables y sus valores
+        int a = 20;
+        int b = 10;
+        //Salida a consola y sus operaciones booleanas
         System.out.println("A y B son iguales es " + (a == b));
         System.out.println("A es igual o mayor que B es " + (a >= b));
         System.out.println("A es igual o mayor que B es " + (a <= b));
