5ce850e83303c68cdf9d407d9d0a2de155e59299
Darksempaii <cesar_enoc666@hotmail.com>
2020-09-20 23:07:47 -0500

    Practica de Opeeadores de Asignacion 1 (sujeto a combios)

--- FProgramacion/src/main/java/com/darksempaii/fprogramacion/OperadoresAsignacion.java
+++ FProgramacion/src/main/java/com/darksempaii/fprogramacion/OperadoresAsignacion.java
@@ -1,19 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.darksempaii.fprogramacion;
 
-/**
- *
- * @author Darksempaii
- */
+
 public class OperadoresAsignacion {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
         int a = 20, b = 10, c, d, e = 10, f = 4, g = 9;
         a = (a + 1);
