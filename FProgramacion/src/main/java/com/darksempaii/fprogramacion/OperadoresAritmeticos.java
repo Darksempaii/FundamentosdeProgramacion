67a164922b55bb421c591fa8a36ec15f1d195d34
Darksempaii <cesar_enoc666@hotmail.com>
2020-09-21 16:47:08 -0500

    Practica de Opeeadores de Aritmeticos v1.2

--- FProgramacion/src/main/java/com/darksempaii/fprogramacion/OperadoresAritmeticos.java
+++ FProgramacion/src/main/java/com/darksempaii/fprogramacion/OperadoresAritmeticos.java
@@ -6,12 +6,15 @@ public class OperadoresAritmeticos {
 
 
     public static void main(String[] args) {
+        //declaracion de variables
        int a, b, c;
+       //Las operaciones Aritmeticas
        a = 3*5+6+1;
-       System.out.println("El valor de A es "+ a);
        b = 3*(5+6)+1;
-       System.out.println("El valor de B es "+ b);
        c = 3*((5+6)*2)+1;
+       //Salida a consola
+       System.out.println("El valor de A es "+ a);
+       System.out.println("El valor de B es "+ b);
        System.out.println("El valor de C es "+ c);
     }
     
