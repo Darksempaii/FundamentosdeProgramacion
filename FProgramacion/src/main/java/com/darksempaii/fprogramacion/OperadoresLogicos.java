0270119cc7467f16db851f35db51ed1f0595763f
Darksempaii <cesar_enoc666@hotmail.com>
2020-09-21 08:11:06 -0500

    Practica de Operadores Logicos 1

--- FProgramacion/src/main/java/com/darksempaii/fprogramacion/OperadoresLogicos.java
+++ FProgramacion/src/main/java/com/darksempaii/fprogramacion/OperadoresLogicos.java
@@ -1,21 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.darksempaii.fprogramacion;
 
-/**
- *
- * @author Darksempaii
- */
+
 public class OperadoresLogicos {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int a = 5;
+        int b = 3;
+        System.out.println("1.La operacion ( a * b + 2 == 17 ) da a: " + (a * b + 2 == 17));
+        System.out.println("2.la operacion (a > b )&&( 2*a <= b) da a: " + ((a > b) && (2 * a <= b)));
+        System.out.println("3.la operacion (b++ > 3) || ((a+b) <= 8) && (a>b) da a: " + ((b++ > 3) || ((a + b) <= 8) && (a > b)));
+        System.out.println("4.la operacion (((a++ /2) < b) && (a++/2 > b)) da a: " + ((((a++ / 2) < b) && (a++ / 2 > b))));
+        System.out.println("5.la operacion ((a<=(2*b)) &&(a>b) || (a*3 <= b)) da a: " + (((a <= (2 * b)) && (a > b) || (a * 3 <= b))));
     }
-    
+
 }
