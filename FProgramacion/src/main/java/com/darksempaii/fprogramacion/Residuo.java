e4b16e968e89179d33788953edee9aeb3f9e581b
Darksempaii <cesar_enoc666@hotmail.com>
2020-09-21 17:09:36 -0500

    Practica deL Residuo de la division v2

--- FProgramacion/src/main/java/com/darksempaii/fprogramacion/Residuo.java
+++ FProgramacion/src/main/java/com/darksempaii/fprogramacion/Residuo.java
@@ -1,37 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.darksempaii.fprogramacion;
 
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
-/**
- *
- * @author Darksempaii
- */
 public class Residuo {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         //Variables
-        float n1, n2;
         float c, r;
         //Entrada
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Inserte el dato: ");
-        n1 = sc.nextInt();
-        System.out.print("Ingrese otro dato: ");
-        n2 = sc.nextInt();
+        String num1 = JOptionPane.showInputDialog("Ingrese el dividendo");
+        String num2 = JOptionPane.showInputDialog("Ingrese el divisor");
+        //conversion de String a Float
+        float num3 = Float.parseFloat(num1);
+        float num4 = Float.parseFloat(num2);
         //Proceso
-        c = n1 / n1;
-        r = n1 % n2;
+        c = num3 / num4;
+        r = num3 % num3;
         //Salida
-        System.out.println("El conciente es: " + c);
-        System.out.println("El residuo es: " + r);
+        JOptionPane.showMessageDialog(null, "El conciente es: " + c + " y su residuo es de: " + r);
     }
 
 }
