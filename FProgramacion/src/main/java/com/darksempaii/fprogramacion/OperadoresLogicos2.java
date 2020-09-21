f80719ec2022803f03b3c0dce6f7c184d550f567
Darksempaii <cesar_enoc666@hotmail.com>
2020-09-20 23:05:30 -0500

    Practica de operadores logicos 1 (sujeto a combios)

--- FProgramacion/src/main/java/com/darksempaii/fprogramacion/OperadoresLogicos2.java
+++ FProgramacion/src/main/java/com/darksempaii/fprogramacion/OperadoresLogicos2.java
@@ -1,28 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.darksempaii.fprogramacion;
 
-/**
- *
- * @author Darksempaii
- */
 public class OperadoresLogicos2 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        int a; a=20;
-        int b; b=10;
-        System.out.println("A y B son iguales es "+ (a==b));
-        System.out.println("A es igual o mayor que B es "+ (a>=b));
-        System.out.println("A es igual o mayor que B es "+ (a<=b));
-        System.out.println("A es mayor que B es "+ (a>b));
-        System.out.println("A es munor que B es "+ (a<b));
-        System.out.println("A es diferente a B es "+ (a!=b));
+        int a;
+        a = 20;
+        int b;
+        b = 10;
+        System.out.println("A y B son iguales es " + (a == b));
+        System.out.println("A es igual o mayor que B es " + (a >= b));
+        System.out.println("A es igual o mayor que B es " + (a <= b));
+        System.out.println("A es mayor que B es " + (a > b));
+        System.out.println("A es munor que B es " + (a < b));
+        System.out.println("A es diferente a B es " + (a != b));
     }
-    
+
 }
