10ef28a6369080b8531282a22fd04690fcf59635
Darksempaii <cesar_enoc666@hotmail.com>
2020-09-21 15:22:24 -0500

    Practica de entrada y salida de datos con JOptionPanel

--- FProgramacion/src/main/java/com/darksempaii/fprogramacion/EntradadeDatos.java
+++ FProgramacion/src/main/java/com/darksempaii/fprogramacion/EntradadeDatos.java
@@ -1,21 +1,18 @@
-
 package com.darksempaii.fprogramacion;
-
+//Clases importadas para el funcionamiento del programa
 import static java.lang.Integer.parseInt;
 import javax.swing.*;
 
-
 public class EntradadeDatos {
 
-
     public static void main(String[] args) {
+        //Entrada grafica
         String nombre_usuario = JOptionPane.showInputDialog("Introduce tu nombre por favor");
         String edad = JOptionPane.showInputDialog("Introduce tu nombre por favor");
-        int edad_usuario;
-        edad_usuario = parseInt(edad);
-        edad_usuario++;
-        System.out.println("hola " + nombre_usuario + " el siguiente año tendras " + edad_usuario + " años");
-        JOptionPane.showMessageDialog(null, "hola " + nombre_usuario + " el siguiente año tendras " + edad_usuario + " años");
+        //conversion a de la variable
+        int edad_usuario = parseInt(edad);
+        //salida grafica
+        JOptionPane.showMessageDialog(null, "Hola " + nombre_usuario + " el siguiente año tendras " + (edad_usuario + 1) + " años.");
     }
-    
+
 }
