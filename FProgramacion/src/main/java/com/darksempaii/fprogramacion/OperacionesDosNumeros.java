606986f01f8003368539c29608209b064ab99c0a
Darksempaii <cesar_enoc666@hotmail.com>
2020-09-21 00:23:30 -0500

    Practica deL scanner (incompleta)

--- FProgramacion/src/main/java/com/darksempaii/fprogramacion/OperacionesDosNumeros.java
+++ FProgramacion/src/main/java/com/darksempaii/fprogramacion/OperacionesDosNumeros.java
@@ -1,21 +1,30 @@
-
 package com.darksempaii.fprogramacion;
+
 import java.util.Scanner;
 
 public class OperacionesDosNumeros {
 
-
     public static void main(String[] args) {
-         Scanner sc = new Scanner(System.in);
-         
-        String mensaje;
-        System.out.print("Cual es el mensaje: ");
-        mensaje=sc.nextLine();
-        System.out.println("El mensaje de hoy es ** "+mensaje+" **");
-    int num1;
-        System.out.println("Dame un numero: " );
-        num1=sc.nextInt();
-        System.out.println("El numero que me diste es: "+ num1);
+        Scanner sc = new Scanner(System.in);
+        int num1;
+        int num2;
+        int suma;
+        int resta;
+        int division;
+        int multiplicacion;
+        String resultado;
+        System.out.println("suma, resta, multiplicacion, divicion de numeros");
+        System.out.println("Dame un numero: ");
+        num1 = sc.nextInt();
+        System.out.println("Dame otro numero: ");
+        num2 = sc.nextInt();
+        suma = num1 + num2;
+        resta = num1 - num2;
+        multiplicacion = num1 * num2;
+        division = num1 / num2;
+        System.out.println("Introdusca la operacion a realizar(suma, resta, multiplicacion, division): ");
+        resultado = sc.nextLine();
+        System.out.println("El resultado de la divicion es: " + division);
     }
-    
+
 }
