dc8e406b933fd3392feeb5f5cff7e1b7fe55497a
Darksempaii <cesar_enoc666@hotmail.com>
2020-09-23 14:06:54 -0500

    Practica del Area del circulo v2.1 (corrección menor de errores)

--- FProgramacion/src/main/java/com/darksempaii/fprogramacion/AreaCirculo.java
+++ FProgramacion/src/main/java/com/darksempaii/fprogramacion/AreaCirculo.java
@@ -2,6 +2,7 @@
 package com.darksempaii.fprogramacion;
 
 import javax.swing.JOptionPane;
+import java.text.DecimalFormat;
 
 
 public class AreaCirculo {
@@ -10,14 +11,13 @@ public class AreaCirculo {
     public static void main(String[] args) {
         //declaracion de PI
         double area=Math.PI;
-        //Entrada Grafica
-        String radio1 = JOptionPane.showInputDialog("Ingrese el radio del circulo");
-        //Conversion de String a Double
-        double radio2 = Double.parseDouble(radio1);
+        //Entrada Grafica y Conversion de String a Double
+        double radio2 = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el radio del circulo"));
         //Operacion Aritmetica
         area=area*radio2*radio2;
-        //Salida grafica
-        JOptionPane.showMessageDialog(null, "El area del circulo es: " + area);
+        //Formato y Salida grafica
+        DecimalFormat df = new DecimalFormat("#0.00");
+        JOptionPane.showMessageDialog(null, "El area del circulo es: " + df.format(area));
                 
     }
 
