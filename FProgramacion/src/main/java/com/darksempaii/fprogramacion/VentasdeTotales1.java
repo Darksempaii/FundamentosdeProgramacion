fdcc76d0ac55b0139b6193ae86b6ad2b6200909c
Darksempaii <cesar_enoc666@hotmail.com>
2020-09-23 17:08:15 -0500

    Practica de Total de ventas v1.2 (re-estructuracion del codigo)

--- FProgramacion/src/main/java/com/darksempaii/fprogramacion/VentasdeTotales1.java
+++ FProgramacion/src/main/java/com/darksempaii/fprogramacion/VentasdeTotales1.java
@@ -5,18 +5,15 @@ import javax.swing.JOptionPane;
 public class VentasdeTotales1 {
 
     public static void main(String[] args) {
-        //Entrada Grafica.
+        //Entrada Grafica y Conversion de String a Integer
         String nombre_vendedor = JOptionPane.showInputDialog("Ingresa un nombre");
-        String a1 = JOptionPane.showInputDialog("Ingresa la cantidad vendida del producto A");
-        String b1 = JOptionPane.showInputDialog("Ingresa la cantidad vendida del producto B");
-        String c1 = JOptionPane.showInputDialog("Ingresa la cantidad vendida del producto C");
-        //Conversion de String a Integer y sus operaciones aritmeticas.
-        int a2 = 200 * Integer.parseInt(a1);
-        int b2 = 500 * Integer.parseInt(b1);
-        int c2 = 200 * Integer.parseInt(c1);
-        int total = a2 + b2 + c2;
+        int a = Integer.parseInt(JOptionPane.showInputDialog("Ingresa la cantidad vendida del producto A"));
+        int b = Integer.parseInt(JOptionPane.showInputDialog("Ingresa la cantidad vendida del producto B"));
+        int c = Integer.parseInt(JOptionPane.showInputDialog("Ingresa la cantidad vendida del producto C"));
+        //operacion aritmetica
+        int total = (200*a) + (500*b) + (350*c);
         //Salida grafica.
-        JOptionPane.showMessageDialog(null, "El vendedor " + nombre_vendedor + " vendio " + a1 + " unidades del producto A, " + b1 + " unidades del producto B, " + c1 + " unidades del producto C y su ganancia total fue de " + total + " Pesos Mexicanos");
+        JOptionPane.showMessageDialog(null, "El vendedor " + nombre_vendedor + " vendio " + a + " unidades del producto A, " + b + " unidades del producto B, " + c + " unidades del producto C y su ganancia total fue de " + total + " Pesos Mexicanos");
 
     }
 
