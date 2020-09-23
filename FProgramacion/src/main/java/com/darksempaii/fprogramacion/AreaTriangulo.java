47fe8fcd4380156ce8ebde66542d854efd8d7aae
Darksempaii <cesar_enoc666@hotmail.com>
2020-09-23 14:11:28 -0500

    Practica del Area del Triangulo v2.2 (corrección menor de errores)

--- FProgramacion/src/main/java/com/darksempaii/fprogramacion/AreaTriangulo.java
+++ FProgramacion/src/main/java/com/darksempaii/fprogramacion/AreaTriangulo.java
@@ -1,20 +1,19 @@
 package com.darksempaii.fprogramacion;
 
-import javax.swing.JOptionPane;
+import java.text.*;
+import javax.swing.*;
 
 public class AreaTriangulo {
     
     public static void main(String[] args) {
-        //Entrada Grafica
-        String base1 = JOptionPane.showInputDialog("Ingrese la base del Triangulo");
-        String altura1 = JOptionPane.showInputDialog("Ingrese la altura del Triangulo");
-        //conversion de String a Double
-        double altura2 = Double.parseDouble(altura1);
-        double base2 = Double.parseDouble(base1);
+        //Entrada Grafica y conversion de String a Double
+        double base = Double.parseDouble(JOptionPane.showInputDialog("Ingrese la base del Triangulo"));
+        double altura = Double.parseDouble(JOptionPane.showInputDialog("Ingrese la altura del Triangulo"));
         //Operacion Aritmetica
-        double area = (base2 * altura2) / 2;
-        //Salida grafica
-        JOptionPane.showMessageDialog(null, "El area del Triangulo es " + area);
+        double area = (base * altura) / 2;
+        //Formato y Salida grafica
+        DecimalFormat df = new DecimalFormat("#0.00");
+        JOptionPane.showMessageDialog(null, "El area del Triangulo es " + df.format(area));
     }
     
 }
