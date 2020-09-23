6907773901d40269a0737535d472afddc0f45d6e
Darksempaii <cesar_enoc666@hotmail.com>
2020-09-23 16:22:46 -0500

    Practica de Entrada de datos v1.1 (optimizacion de codigo)

--- FProgramacion/src/main/java/com/darksempaii/fprogramacion/EntradadeDatos.java
+++ FProgramacion/src/main/java/com/darksempaii/fprogramacion/EntradadeDatos.java
@@ -7,9 +7,8 @@ public class EntradadeDatos {
     public static void main(String[] args) {
         //Entrada grafica
         String nombre_usuario = JOptionPane.showInputDialog("Introduce tu nombre por favor");
-        String edad = JOptionPane.showInputDialog("Introduce tu edad por favor");
         //conversion a de la variable y su porterior manipulacion
-        int edad_usuario = Integer.parseInt(edad);
+        int edad_usuario = Integer.parseInt(JOptionPane.showInputDialog("Introduce tu edad por favor"));
         edad_usuario++;
         //salida grafica
         JOptionPane.showMessageDialog(null, "Hola " + nombre_usuario + " el siguiente año tendras " + edad_usuario + " años.");
