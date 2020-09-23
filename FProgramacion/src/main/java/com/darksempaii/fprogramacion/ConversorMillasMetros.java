3bd9341e9b2854d9312b601b314e93d54511f0f3
Darksempaii <cesar_enoc666@hotmail.com>
2020-09-23 14:22:14 -0500

    Practica del Conversion millas a metros v2.2 (mejoras a mostrar los resultados)

--- FProgramacion/src/main/java/com/darksempaii/fprogramacion/ConversorMillasMetros.java
+++ FProgramacion/src/main/java/com/darksempaii/fprogramacion/ConversorMillasMetros.java
@@ -2,18 +2,19 @@
 package com.darksempaii.fprogramacion;
 
 //Clases Importadas
-import javax.swing.JOptionPane;
+import java.text.*;
+import javax.swing.*;
 
 public class ConversorMillasMetros {
 
     public static void main(String[] args) {
-        //Entrada Grafica
-        String distancia1 = JOptionPane.showInputDialog("Ingrese la distancia en millas");
-        //Conversion de String a Double y Operacion Aritmetica
-        double distancia2 = Double.parseDouble(distancia1);
-        double metros = distancia2 * 1609;
-        //Salida Gafica
-        JOptionPane.showMessageDialog(null, "La distancia en Metros es: " +metros);
+        //Entrada Grafica y Conversion de String a Double
+        double distancia = Double.parseDouble(JOptionPane.showInputDialog("Ingrese la distancia en millas"));
+        //Operacion Aritmetica
+        double metros = distancia * 1609;
+        //Formato y Salida Gafica
+        DecimalFormat df = new DecimalFormat("#0.0");
+        JOptionPane.showMessageDialog(null, "La distancia en Metros es: " + df.format(metros));
     }
 
 }
