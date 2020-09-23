5c0b3156e9bea9b38a173fb5da81f50ddd1bd85a
Darksempaii <cesar_enoc666@hotmail.com>
2020-09-23 16:31:48 -0500

    Practica del Scanner v1.2 (optimizacion de codigo)

--- FProgramacion/src/main/java/com/darksempaii/fprogramacion/OperacionesDosNumeros.java
+++ FProgramacion/src/main/java/com/darksempaii/fprogramacion/OperacionesDosNumeros.java
@@ -5,25 +5,24 @@ import java.util.Scanner;
 public class OperacionesDosNumeros {
 
     public static void main(String[] args) {
+        //Declaracion del Scanner y variables principales
         Scanner sc = new Scanner(System.in);
-        int num1;
-        int num2;
-        int suma;
-        int resta;
-        int division;
-        int multiplicacion;
+        double num1;
+        double num2;
+        //Entrada scanner
         System.out.println("suma, resta, multiplicacion, divicion de numeros");
         System.out.print("Dame un numero: ");
         num1 = sc.nextInt();
         System.out.print("Dame otro numero: ");
         num2 = sc.nextInt();
-        suma = num1 + num2;
+        //Operaciones Aritmeticas y Salida a Consola
+        double suma = num1 + num2;
         System.out.println("El resultado de la divicion es: " + suma);
-        resta = num1 - num2;
+        double resta = num1 - num2;
         System.out.println("El resultado de la divicion es: " + resta);
-        multiplicacion = num1 * num2;
+        double multiplicacion = num1 * num2;
         System.out.println("El resultado de la divicion es: " + multiplicacion);
-        division = num1 / num2;
+        double division = num1 / num2;
         System.out.println("El resultado de la divicion es: " + division);
     }
 
