445c1dd3f66d64f9f63ef0e52b2139a1505336ac
Darksempaii <cesar_enoc666@hotmail.com>
2020-09-30 16:06:29 -0500

    Programa evsluador de edad v1.9 (Act3.1)

--- Activ31ifelse/src/main/java/com/darksempaii/activ31ifelse/EvaluaEdad.java
+++ Activ31ifelse/src/main/java/com/darksempaii/activ31ifelse/EvaluaEdad.java
@@ -5,12 +5,17 @@ import javax.swing.*;
 public class EvaluaEdad {
 
     public static void main(String[] args) {
-        int edad = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el radio del circulo"));
-        if (edad >= 18) {
-            JOptionPane.showMessageDialog(null, "Eres mayor de Edad");
+        int edad;
+        do{
+        edad = Integer.parseInt(JOptionPane.showInputDialog(null,"Ingrese el radio del circulo","Dato",JOptionPane.QUESTION_MESSAGE));
+        if (edad >= 0 && edad <= 17) {
+            JOptionPane.showMessageDialog(null, "Eres menor de Edad","Respuesta", JOptionPane.INFORMATION_MESSAGE);
+        } else if (edad >= 18) {
+            JOptionPane.showMessageDialog(null, "Eres mayor de Edad","Respuesta", JOptionPane.INFORMATION_MESSAGE);
         } else {
-            JOptionPane.showMessageDialog(null, "Eres menor de Edad");
+            JOptionPane.showMessageDialog(null, "A ingresado una edad incorrecta.\nPor favor intentalo de nuevo", "Error", JOptionPane.ERROR_MESSAGE);
         }
+        }while(edad < 0);
 
     }
 
