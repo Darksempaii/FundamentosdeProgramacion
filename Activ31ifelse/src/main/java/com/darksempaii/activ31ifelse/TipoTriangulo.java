590e2226cd59578877c5cb1603c976408e6cc11e
Darksempaii <cesar_enoc666@hotmail.com>
2020-09-30 15:52:58 -0500

    programa que identificador de tipos de triangulo v1.5 (Act3.1)

--- Activ31ifelse/src/main/java/com/darksempaii/activ31ifelse/TipoTriangulo.java
+++ Activ31ifelse/src/main/java/com/darksempaii/activ31ifelse/TipoTriangulo.java
@@ -5,15 +5,15 @@ import javax.swing.*;
 public class TipoTriangulo {
 
     public static void main(String[] args) {
-        double num1 = Double.parseDouble(JOptionPane.showInputDialog("Ingrese las medidas del lado A"));
-        double num2 = Double.parseDouble(JOptionPane.showInputDialog("Ingrese las medidas del lado B"));
-        double num3 = Double.parseDouble(JOptionPane.showInputDialog("Ingrese las medidas del lado C"));
+        double num1 = Double.parseDouble(JOptionPane.showInputDialog(null,"Ingrese las medidas del lado A","Lado A",JOptionPane.QUESTION_MESSAGE));
+        double num2 = Double.parseDouble(JOptionPane.showInputDialog(null,"Ingrese las medidas del lado B","Lado B",JOptionPane.QUESTION_MESSAGE));
+        double num3 = Double.parseDouble(JOptionPane.showInputDialog(null,"Ingrese las medidas del lado C","Lado C",JOptionPane.QUESTION_MESSAGE));
         if (num1 == num2 && num2 == num3) {
-            JOptionPane.showMessageDialog(null, "Es un triangulo Equilatero");
+            JOptionPane.showMessageDialog(null, "Es un triangulo Equilatero","Resultado", JOptionPane.INFORMATION_MESSAGE);
         } else if (num1 == num2 || num1 == num3 || num2 == num3) {
-            JOptionPane.showMessageDialog(null, "Es un triangulo Isoceles");
+            JOptionPane.showMessageDialog(null, "Es un triangulo Isoceles","Resultado", JOptionPane.INFORMATION_MESSAGE);
         } else if(num1!=num2 || num1!=num3 || num3!=num2){
-            JOptionPane.showMessageDialog(null, "Es un triangulo Escaleno");
+            JOptionPane.showMessageDialog(null, "Es un triangulo Escaleno","Resultado", JOptionPane.INFORMATION_MESSAGE);
         }
 
     }
