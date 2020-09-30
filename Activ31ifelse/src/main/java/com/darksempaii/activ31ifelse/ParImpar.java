4a4ce355c3d2a0154f8bcaa1024940a323dacbda
Darksempaii <cesar_enoc666@hotmail.com>
2020-09-30 16:09:55 -0500

    Programa identificador de nmumeros Pares v1.5 (Act3.1)

--- Activ31ifelse/src/main/java/com/darksempaii/activ31ifelse/ParImpar.java
+++ Activ31ifelse/src/main/java/com/darksempaii/activ31ifelse/ParImpar.java
@@ -5,12 +5,12 @@ import javax.swing.*;
 public class ParImpar {
 
     public static void main(String[] args) {
-        int numero = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un numero entero"));
+        int numero = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese un numero entero", "Dato", JOptionPane.QUESTION_MESSAGE));
 
         if (numero % 2 == 0) {
-            JOptionPane.showMessageDialog(null, "El numero es Par");
+            JOptionPane.showMessageDialog(null, "El numero es Par", "Resultado", JOptionPane.INFORMATION_MESSAGE);
         } else {
-            JOptionPane.showMessageDialog(null, "El numero es Impar");
+            JOptionPane.showMessageDialog(null, "El numero es Impar", "Resultado", JOptionPane.INFORMATION_MESSAGE);
         }
     }
 
