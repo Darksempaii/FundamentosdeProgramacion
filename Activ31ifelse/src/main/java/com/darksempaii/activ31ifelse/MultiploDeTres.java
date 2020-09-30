0809741f4755a4295fc15c979710b523a7ed66b4
Darksempaii <cesar_enoc666@hotmail.com>
2020-09-30 15:56:37 -0500

    programa identificador de nmumeros multiplo de 3 v1.5 (Act3.1)

--- Activ31ifelse/src/main/java/com/darksempaii/activ31ifelse/MultiploDeTres.java
+++ Activ31ifelse/src/main/java/com/darksempaii/activ31ifelse/MultiploDeTres.java
@@ -6,13 +6,13 @@ import javax.swing.*;
 public class MultiploDeTres {
 
     public static void main(String[] args) {
-                int numero = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un numero entero"));
+        int numero = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese un numero entero", "Dato", JOptionPane.QUESTION_MESSAGE));
 
         if (numero % 3 == 0) {
-            JOptionPane.showMessageDialog(null, "El numero es multiplo de 3");
+            JOptionPane.showMessageDialog(null, "El numero es multiplo de 3","Resultado", JOptionPane.INFORMATION_MESSAGE);
         } else {
-            JOptionPane.showMessageDialog(null, "El numero es no es multiplo de 3");
+            JOptionPane.showMessageDialog(null, "El numero es no es multiplo de 3","Resultado", JOptionPane.INFORMATION_MESSAGE);
         }
     }
-    
+
 }
