64ac4f58d519696a646ff70745f3f4ea6a6c5fff
Darksempaii <cesar_enoc666@hotmail.com>
2020-09-30 13:01:06 -0500

    programa que identificador de tipos de triangulo v1.1 (Act3.1)

--- Activ31ifelse/src/main/java/com/darksempaii/activ31ifelse/TipoTriangulo.java
+++ Activ31ifelse/src/main/java/com/darksempaii/activ31ifelse/TipoTriangulo.java
@@ -1,21 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.darksempaii.activ31ifelse;
 
-/**
- *
- * @author Darksempaii
- */
+import javax.swing.*;
+
 public class TipoTriangulo {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        double num1 = Double.parseDouble(JOptionPane.showInputDialog("Ingrese las medidas del lado A"));
+        double num2 = Double.parseDouble(JOptionPane.showInputDialog("Ingrese las medidas del lado B"));
+        double num3 = Double.parseDouble(JOptionPane.showInputDialog("Ingrese las medidas del lado C"));
+        if (num1 == num2 && num2 == num3) {
+            JOptionPane.showMessageDialog(null, "Es un triangulo Equilatero");
+        } else if (num1 == num2 || num1 == num3 || num2 == num3) {
+            JOptionPane.showMessageDialog(null, "Es un triangulo Isoceles");
+        } else if(num1!=num2 || num1!=num3 || num3!=num2){
+            JOptionPane.showMessageDialog(null, "Es un triangulo Escaleno");
+        }
+
     }
-    
+
 }
