4cdf028462557385100c24c747f5ba94519de5ee
Darksempaii <cesar_enoc666@hotmail.com>
2020-09-29 19:00:44 -0500

    Isentificador de nmumeros Pares e Impares v1.1 (Act3.1)

--- Activ31ifelse/src/main/java/com/darksempaii/activ31ifelse/ParImpar.java
+++ Activ31ifelse/src/main/java/com/darksempaii/activ31ifelse/ParImpar.java
@@ -1,21 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.darksempaii.activ31ifelse;
 
-/**
- *
- * @author Darksempaii
- */
+import javax.swing.*;
+
 public class ParImpar {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int numero = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un numero entero"));
+
+        if (numero % 2 == 0) {
+            JOptionPane.showMessageDialog(null, "El numero es Par");
+        } else {
+            JOptionPane.showMessageDialog(null, "El numero es Impar");
+        }
     }
-    
+
 }
