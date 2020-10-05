a9fcd8cbeb43bf265d0687b5b1c5d5fdefa8baa9
Darksempaii <cesar_enoc666@hotmail.com>
2020-10-05 10:31:54 -0500

    Programa Areas(Act 3.2)

--- Activ32switch/src/main/java/com/darksempaii/activ32switch/AreasConCase.java
+++ Activ32switch/src/main/java/com/darksempaii/activ32switch/AreasConCase.java
@@ -1,21 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.darksempaii.activ32switch;
 
-/**
- *
- * @author Darksempaii
- */
+import javax.swing.*;
+
 public class AreasConCase {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int figura = Integer.parseInt(JOptionPane.showInputDialog(null, "Elige una opcion: \n1: Cuadrado \n2: Rectangulo \n3: Triangulo \n4: Circulo", "Selector", JOptionPane.QUESTION_MESSAGE));
+        int lado,base,altura ;
+          
+        switch (figura) {
+            case 1:
+                lado = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el lado", "Introduce", JOptionPane.QUESTION_MESSAGE));
+                JOptionPane.showMessageDialog(null, "El area del cuedrado es "+ Math.pow(lado, 2), "Resultado", JOptionPane.INFORMATION_MESSAGE);
+                break;
+            case 2:
+                base = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese la Base", "Introduce", JOptionPane.QUESTION_MESSAGE));
+                altura = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese la Altura", "Introduce", JOptionPane.QUESTION_MESSAGE));
+                JOptionPane.showMessageDialog(null, "El area del Rectangulo es "+(base*altura), "Resultado", JOptionPane.INFORMATION_MESSAGE);
+                break;
+            case 3:
+                base = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese la Base", "Introduce", JOptionPane.QUESTION_MESSAGE));
+                altura = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese la Altura", "Introduce", JOptionPane.QUESTION_MESSAGE));
+                JOptionPane.showMessageDialog(null, "El area del Rectangulo es "+(base*altura)/2, "Resultado", JOptionPane.INFORMATION_MESSAGE);
+                break;
+            case 4:
+                double radio = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el radio del circulo"));
+                JOptionPane.showMessageDialog(null, "El area del Rectangulo es "+Math.PI*(Math.pow(radio,2)), "Resultado", JOptionPane.INFORMATION_MESSAGE);
+                break;
+
+            default:
+                throw new AssertionError();
+        }
+
     }
-    
+
 }
