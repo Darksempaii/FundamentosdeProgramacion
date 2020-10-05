8b2e5904003f8c3642998dffc95635a2af2fd70d
Darksempaii <cesar_enoc666@hotmail.com>
2020-10-05 11:04:11 -0500

    Programa Calculadora(Act 3.2)

--- Activ32switch/src/main/java/com/darksempaii/activ32switch/Calculadora.java
+++ Activ32switch/src/main/java/com/darksempaii/activ32switch/Calculadora.java
@@ -1,21 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.darksempaii.activ32switch;
 
-/**
- *
- * @author Darksempaii
- */
+import javax.swing.*;
+import java.text.*;
+
 public class Calculadora {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int calculo = Integer.parseInt(JOptionPane.showInputDialog(null, "Elige una opcion: \n1: Suma \n2: Resta \n3: Multiplicacion \n4: Division", "Selector", JOptionPane.QUESTION_MESSAGE));
+        double a, b;
+        DecimalFormat df = new DecimalFormat("#0.000");
+        switch (calculo) {
+            case 1:
+                a = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el primer numero", "Introduce", JOptionPane.QUESTION_MESSAGE));
+                b = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el segundo numero", "Introduce", JOptionPane.QUESTION_MESSAGE));
+                JOptionPane.showMessageDialog(null, "El resultado de la Suma es " + df.format(a + b), "Suma", JOptionPane.INFORMATION_MESSAGE);
+                break;
+            case 2:
+                a = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el primer numero", "Introduce", JOptionPane.QUESTION_MESSAGE));
+                b = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el segundo numero", "Introduce", JOptionPane.QUESTION_MESSAGE));
+                JOptionPane.showMessageDialog(null, "El resultado de la Resta es " + df.format(a - b), "Resta", JOptionPane.INFORMATION_MESSAGE);
+                break;
+            case 3:
+                a = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el primer numero", "Introduce", JOptionPane.QUESTION_MESSAGE));
+                b = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el segundo numero", "Introduce", JOptionPane.QUESTION_MESSAGE));
+                JOptionPane.showMessageDialog(null, "El resultado de la multiplicacion es " + df.format(a * b), "Multiplicacion", JOptionPane.INFORMATION_MESSAGE);
+                break;
+            case 4:
+                a = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el primer numero", "Introduce", JOptionPane.QUESTION_MESSAGE));
+                b = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el segundo numero", "Introduce", JOptionPane.QUESTION_MESSAGE));
+                JOptionPane.showMessageDialog(null, "El resultado de la Division es " + df.format(a / b), "Division", JOptionPane.INFORMATION_MESSAGE);
+                break;
+            default:
+                JOptionPane.showMessageDialog(null, "La opcion ingresada es incorrecta.\nEjecute el programa de nuevo", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+
     }
-    
+
 }
