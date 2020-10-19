4afff24506fc9af284c5874d96b44d4233180c85
Darksempaii <cesar_enoc666@hotmail.com>
2020-10-19 10:54:10 -0500

    Comprobacion de mail (Act 3.5)

--- Activ35-for/src/For/ComprobacionMail.java
+++ Activ35-for/src/For/ComprobacionMail.java
@@ -1,21 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package For;
 
-/**
- *
- * @author Darksempaii
- */
+import javax.swing.JOptionPane;
+
+
 public class ComprobacionMail {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        boolean arroba=false;
+        String mail =JOptionPane.showInputDialog(null,"Ingresa tu direccion de correo","e-Mail",JOptionPane.QUESTION_MESSAGE);
+        for (int i = 0; i < mail.length(); i++) {
+            if (mail.charAt(i)=='@') {
+                arroba=true;
+            }
+        }
+        if (arroba == true) {
+            JOptionPane.showMessageDialog(null, "Es correcto","e-Mail", JOptionPane.INFORMATION_MESSAGE);
+        }else {
+            JOptionPane.showMessageDialog(null, "No es correcto","e-Mail", JOptionPane.ERROR_MESSAGE);
+        }
     }
     
 }
