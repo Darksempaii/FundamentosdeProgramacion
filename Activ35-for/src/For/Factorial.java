63216353b66e5efe60eea1717de3d99880b0523e
Darksempaii <cesar_enoc666@hotmail.com>
2020-10-20 10:33:40 -0500

    Factoriales con BigInteger (Act 3.5)

--- Activ35-for/src/For/Factorial.java
+++ Activ35-for/src/For/Factorial.java
@@ -1,21 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package For;
 
-/**
- *
- * @author Darksempaii
- */
-public class Factorial {
+import javax.swing.JOptionPane;
+import java.math.BigInteger;
 
-    /**
-     * @param args the command line arguments
-     */
+public class Factorial {
+  
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Variable y Creacion de objeto BigInteger iniciado en 1
+        String numero = JOptionPane.showInputDialog(null, "Introduce un numero", "Factorial", JOptionPane.QUESTION_MESSAGE);
+        BigInteger var = new BigInteger("1");
+        //Ciclo For y Operacion cin BigInterger
+        for (int i = Integer.parseInt(numero); i > 0; i--) {
+            var = var.multiply(new BigInteger(Integer.toString(i)));
+        }
+        //Mensaje de repuesta y salida del programa
+        JOptionPane.showMessageDialog(null, "El factorial de " + numero + " es " + var, "Resultado", JOptionPane.INFORMATION_MESSAGE);
+        System.exit(0);
     }
     
 }
