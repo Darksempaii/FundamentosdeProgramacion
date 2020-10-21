44c111c764737ee6379f14f488fff85eaba482fb
Darksempaii <cesar_enoc666@hotmail.com>
2020-10-21 14:18:53 -0500

    Proyeccion de Poblacion (Act 3.5)

--- Activ35-for/src/For/ProyeccionPoblacionVictoria.java
+++ Activ35-for/src/For/ProyeccionPoblacionVictoria.java
@@ -1,21 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package For;
 
-/**
- *
- * @author Darksempaii
- */
+import javax.swing.JOptionPane;
+
+
 public class ProyeccionPoblacionVictoria {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
+    public static void main(String[] args) throws InterruptedException {
+        //Varables
+        int poblacion = 300000;
+        int incremento = 0;
+        int decremento = 0;
+        int d1, i1, itotal;
+        for (int i = 0; i <= 5; i++) {
+            //Operacion temporal
+            i1 = (poblacion / 100)*5;
+            d1 = (poblacion / 100);
+            //Acumulacion de la temporal
+            incremento = incremento + i1;
+            decremento = decremento + d1;
+            
+            //Operacion Principal
+            poblacion = poblacion + i1 - d1;
+            //Resultado RAW
+            System.out.print(poblacion + " " + i1 + " " + d1);
+            System.out.println(" "+incremento+" "+decremento);
+            //Interrupcion Programada (milesimas de segunto)
+            Thread.sleep(750);
+        }
+        //Resultado en JOptionPane
+        JOptionPane.showMessageDialog(null, "poblacion total es de "+poblacion+"\nIncremento neto de "+incremento+"\nDecremento neto de "+decremento+"\nen los proximos 5 años","Estadistica a 5 años", JOptionPane.INFORMATION_MESSAGE);
+        //Salida del programa
+        System.exit(0);
+    }
+
 }
