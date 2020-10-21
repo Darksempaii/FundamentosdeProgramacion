4b451720a601bbc5cdf3d2f4143398a81b6b9b62
Darksempaii <cesar_enoc666@hotmail.com>
2020-10-21 12:33:05 -0500

    Numero mayor y menor (Act 3.5)

--- Activ35-for/src/For/NumeroMayorMenor.java
+++ Activ35-for/src/For/NumeroMayorMenor.java
@@ -1,21 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package For;
 
-/**
- *
- * @author Darksempaii
- */
+import javax.swing.JOptionPane;
+
 public class NumeroMayorMenor {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int nMayor = 1000000;
+        int nMenor = 0;
+        int numero;
+        for (int i = 1; i <= 5; i++) {
+            numero = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese un numero", "Dato", JOptionPane.QUESTION_MESSAGE));
+            if (nMayor > numero) {
+                nMayor = numero;
+            } 
+            if (nMenor < numero) {
+                nMenor = numero;
+            }
+            System.out.println(nMayor+" "+nMenor+" "+numero);
+        }
+        JOptionPane.showMessageDialog(null, "Numero mayor:" + nMayor + "\nNumero Menor: " + nMenor, "Respuesta", JOptionPane.INFORMATION_MESSAGE);
     }
-    
+
 }
