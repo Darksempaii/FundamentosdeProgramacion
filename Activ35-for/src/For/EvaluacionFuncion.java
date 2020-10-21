eb04891750cd02a95e8f8bc1726ce34fb27aa8dd
Darksempaii <cesar_enoc666@hotmail.com>
2020-10-21 15:41:05 -0500

    Evaluacion de Funcion (Act 3.5)

--- Activ35-for/src/For/EvaluacionFuncion.java
+++ Activ35-for/src/For/EvaluacionFuncion.java
@@ -1,21 +1,53 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package For;
 
-/**
- *
- * @author Darksempaii
- */
 public class EvaluacionFuncion {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws InterruptedException {
+        //variables
+        int y, x, respuesta;
+        //cabezera de la tabla
+        String cabecera = "\n\t función de f(x,y)=x*2+y\n";
+        cabecera += "\nX\tY\tF(x,y)=x*2+y\n";
+        cabecera += "----\t----\t--------------\n";
+        //cabezera a consola
+        System.out.print(cabecera);
+        for (int i = 1; i <= 3; i++) {
+            switch (i) {
+                case 1:
+                    //Parte 1 del proceso
+                    y = 2;
+                    x = 3;
+                    for (int j = 1; j <= 4; j++) {
+                        respuesta = (x * 2 + y);
+                        String tabla = y + "\t" + x + "\t" + respuesta + "\n";
+                        System.out.print(tabla);
+                        x = x + 3;
+                        Thread.sleep(500);
+                    }
+                    break;
+                case 2:
+                    //Parte 2 del proceso
+                    y = 4;
+                    x = 3;
+                    for (int j = 1; j <= 3; j++) {
+                        respuesta = (x * 2 + y);
+                        String tabla = y + "\t" + x + "\t" + respuesta + "\n";
+                        System.out.print(tabla);
+                        x = x + 3;
+                        Thread.sleep(500);
+                    }
+                    break;
+                default:
+                    //Ultima parte
+                    y = 6;
+                    x = 12;
+                    respuesta = (x * 2 + y);
+                    String tabla = y + "\t" + x + "\t" + respuesta + "\n";
+                    System.out.print(tabla);
+                    Thread.sleep(500);
+                    break;
+            }
+        }
+        System.exit(0);
     }
-    
+
 }
