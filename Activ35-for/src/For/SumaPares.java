5865899e516bb469c5dfd5b24ba647f74617633a
Darksempaii <cesar_enoc666@hotmail.com>
2020-10-21 14:26:04 -0500

    Suma de pares v2 (Act 3.5)

--- Activ35-for/src/For/SumaPares.java
+++ Activ35-for/src/For/SumaPares.java
@@ -6,12 +6,15 @@ import javax.swing.JOptionPane;
 public class SumaPares {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         int suma =0;
-        for (int i = 2; i < 100; i+=2) {
+        for (int i = 2; i <= 100; i+=2) {
             suma=suma+i;
+            System.out.println("suma: "+suma+" Numero: "+i);
+            Thread.sleep(200);
         }
         JOptionPane.showMessageDialog(null, "Suma de pares: "+suma,"Respuesta", JOptionPane.INFORMATION_MESSAGE);
+        System.exit(0);
     }
     
 }
