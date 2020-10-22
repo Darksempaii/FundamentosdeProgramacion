0797b4cf5c3c093fc915209ce94e51c807977e4b
Darksempaii <cesar_enoc666@hotmail.com>
2020-10-22 10:37:13 -0500

    Examen del tema 3

--- Examen_Tema3/src/examen_tema3/Examen_Tema3V5.java
+++ Examen_Tema3/src/examen_tema3/Examen_Tema3V5.java
@@ -5,10 +5,12 @@ import javax.swing.JOptionPane;
 public class Examen_Tema3V5 {
 
     public static void main(String[] args) {
+        //Variables Empleadas
         Boolean llave = true;
         int numero, negativo = 0, positivo = 0;
         do {
             numero = Integer.parseInt(JOptionPane.showInputDialog(null, "Introduce tu altura en cm", "Peso Ideal", JOptionPane.QUESTION_MESSAGE));
+            //Comprobador de numeros
             if (numero < 0) {
                 negativo++;
             } else if (numero > 0) {                
@@ -16,8 +18,10 @@ public class Examen_Tema3V5 {
             } else if (numero == 0) {
                 llave = false;
             }
+            //Impresion de los numeros ingresados por el usuario
             System.out.println("numero: "+numero);
         } while (llave);
+        //Resultado final
         JOptionPane.showMessageDialog(null, "Total de numeros negativos: "+negativo+"\nTotal de numeros positivos: "+positivo, "Resultado", JOptionPane.INFORMATION_MESSAGE);
     }
 
