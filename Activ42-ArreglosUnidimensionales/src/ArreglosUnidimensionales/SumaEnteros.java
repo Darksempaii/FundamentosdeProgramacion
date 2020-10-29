68509554665ac473810864b53cddb22de0b0a11a
Darksempaii <cesar_enoc666@hotmail.com>
2020-10-29 10:55:01 -0600

    Suma de enteros (Act 4.2)

--- Activ42-ArreglosUnidimensionales/src/ArreglosUnidimensionales/SumaEnteros.java
+++ Activ42-ArreglosUnidimensionales/src/ArreglosUnidimensionales/SumaEnteros.java
@@ -1,21 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ArreglosUnidimensionales;
 
-/**
- *
- * @author Darksempaii
- */
+
 public class SumaEnteros {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
+    public static void main(String[] args) throws InterruptedException {
+        int[] numeros = {24, 5, 3, 51, 63, 7, 67, 90, 32, 43};
+        int suma = 0;
+        System.out.print("los elementos del arreglo son: ");
+        for (int i = 0; i < numeros.length; i++) {  
+            suma = suma + numeros[i];
+            System.out.print(numeros[i]+" ");
+            Thread.sleep(500);
+        }
+        System.out.println("");
+        System.out.println("El Total de la suma es: " + suma);
+        
+        Thread.sleep(5000);
+        System.exit(0);
+    }
+
 }
