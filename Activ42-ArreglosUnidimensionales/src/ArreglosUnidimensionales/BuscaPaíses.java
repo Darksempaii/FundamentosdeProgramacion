38418104874a6049f9c65b2c955db2109c6202fc
cesar <cesar@DESKTOP-618VJAQ>
2020-11-03 10:56:26 -0600

    Busca Paises (Act 4.2)

--- Activ42-ArreglosUnidimensionales/src/ArreglosUnidimensionales/BuscaPaíses.java
+++ Activ42-ArreglosUnidimensionales/src/ArreglosUnidimensionales/BuscaPaíses.java
@@ -1,11 +1,28 @@
 
 package ArreglosUnidimensionales;
-
+import javax.swing.JOptionPane;
 
 public class BuscaPaíses {
 
     public static void main(String[] args) {
+        //Variables y array,s
         String[] paises = {"Mexico","España","Japon","Corea del sur","Rusia","China"};
+        String nombre = JOptionPane.showInputDialog(null,"Inserta el nombre de un pais"," Buscador",JOptionPane.QUESTION_MESSAGE);
+        int intento= 0;
+        //se cuencia de busqueda
+        for (int i = 0; i < paises.length; i++) {
+           if (nombre.equalsIgnoreCase(paises[i])) {
+                JOptionPane.showMessageDialog(null, "el pais "+paises[i]+" si existe en el indice","Pais encontrado", JOptionPane.INFORMATION_MESSAGE);
+            }else{
+            intento++;
+            } 
+        }
+        //En caso de no existir en el indice
+        if (intento >= paises.length) {
+            JOptionPane.showMessageDialog(null, "el pais "+nombre+" no existe en el indice","Error", JOptionPane.WARNING_MESSAGE);
+        }
+        //salida
+        System.exit(0);
     }
     
 }
