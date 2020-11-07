40cae9196cee3a8b4303dc99f8bc48382ee70fc1
cesar <cesar@DESKTOP-618VJAQ>
2020-11-07 11:31:57 -0600

    Cuenta ceros(Act 4.2)

--- Activ42-ArreglosUnidimensionales/src/ArreglosUnidimensionales/CuentaCeros.java
+++ Activ42-ArreglosUnidimensionales/src/ArreglosUnidimensionales/CuentaCeros.java
@@ -1,21 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ArreglosUnidimensionales;
 
-/**
- *
- * @author Darksempaii
- */
+import javax.swing.JOptionPane;
+
 public class CuentaCeros {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Declaracion del tamaño del arreglo
+        int cantidad = Integer.parseInt(JOptionPane.showInputDialog(null, "Cuantos numeros van a ingresas", "Cantidad", JOptionPane.QUESTION_MESSAGE));
+        //Introduccion de datos de Tipo string
+        int[] numeros = new int[cantidad];
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = Integer.parseInt(JOptionPane.showInputDialog(null, "Engresa tu " + (i + 1) + "º dato", "Cantidad", JOptionPane.QUESTION_MESSAGE));
+        }
+        //Contador de ceros
+        int ceros = 0;
+        int bucle = 0;
+        while (bucle < numeros.length) {
+            String datos = Integer.toString(numeros[bucle]);
+            for (int j = 0; j < datos.length(); j++) {
+                if (datos.charAt(j)=='0') {
+                    ceros++;
+                }
+            }
+            bucle++;
+        }
+        //Salida a consola
+        System.out.println("");
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print(numeros[i] + " ");
+        }
+        System.out.println("");
+        System.out.println("El total de ceros es: " + ceros);
+        //Salida JOptionPane
+        JOptionPane.showMessageDialog(null, "La cantidad de ceros introducidos es: " + ceros, "Resultado", JOptionPane.ERROR_MESSAGE);
     }
-    
+
 }
