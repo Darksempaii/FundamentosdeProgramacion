3c67ab64058930d9e91e6521c6e8994ffba5f808
cesar <cesar@DESKTOP-618VJAQ>
2020-11-06 20:13:06 -0600

    Suma de Pares(Act 4.2)

--- Activ42-ArreglosUnidimensionales/src/ArreglosUnidimensionales/SumaPares.java
+++ Activ42-ArreglosUnidimensionales/src/ArreglosUnidimensionales/SumaPares.java
@@ -1,21 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ArreglosUnidimensionales;
 
-/**
- *
- * @author Darksempaii
- */
+import javax.swing.JOptionPane;
+
 public class SumaPares {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        int cantidad;
+        boolean bucle = false;
+        cantidad = Integer.parseInt(JOptionPane.showInputDialog(null, "Cuantos numeros van a ingresas", "Cantidad", JOptionPane.QUESTION_MESSAGE));
+        int[] numeros = new int[cantidad];
+        for (int i = 0; i < numeros.length; i++) {
+            do {
+                numeros[i] = Integer.parseInt(JOptionPane.showInputDialog(null, "Engresa tu " + (i + 1) + "º dato", "Cantidad", JOptionPane.QUESTION_MESSAGE));
+                System.out.println(numeros[i]%2);
+                if (numeros[i]%2==1) {
+                    JOptionPane.showMessageDialog(null, "Ingresastes un numero que no es par.\nPor favor intentalo de nuevo ","Error", JOptionPane.ERROR_MESSAGE);
+                    bucle = true;
+                } else if (numeros[i]%2 == 0) {
+                    bucle = false;
+                }
+            } while (bucle);
+        }
+        int suma = 0;
+        for (int i = 0; i < numeros.length; i++) {
+            suma = suma + numeros[i];
+        }
+        JOptionPane.showMessageDialog(null, "el resultado de la suma de pares es: "+suma,"Resultado", JOptionPane.ERROR_MESSAGE);
     }
-    
+
 }
