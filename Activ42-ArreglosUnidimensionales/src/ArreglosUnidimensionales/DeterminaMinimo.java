9f8bcc08f263069d84324bfa7b4b94d7575f5a9c
cesar <cesar@DESKTOP-618VJAQ>
2020-11-07 14:49:02 -0600

    Determina minimo(Act 4.2)

--- Activ42-ArreglosUnidimensionales/src/ArreglosUnidimensionales/DeterminaMinimo.java
+++ Activ42-ArreglosUnidimensionales/src/ArreglosUnidimensionales/DeterminaMinimo.java
@@ -1,21 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ArreglosUnidimensionales;
 
-/**
- *
- * @author Darksempaii
- */
+import javax.swing.JOptionPane;
+
 public class DeterminaMinimo {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
+    public static void main(String[] args) {
+        int cantidad = Integer.parseInt(JOptionPane.showInputDialog(null, "Cuantos numeros van a ingresas", "Cantidad", JOptionPane.QUESTION_MESSAGE));
+        int[] numeros = new int[cantidad];
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingresa tu " + (i + 1) + "º dato", "Cantidad", JOptionPane.QUESTION_MESSAGE));
+        }
+        int minimo = 999999999;
+        System.out.print("Los numeros del arreglo son: ");
+        for (int i = 0; i < numeros.length; i++) {
+            if (minimo > numeros[i]) {
+                minimo = numeros[i];
+            }
+            System.out.print(numeros[i]+", ");
+        }
+        System.out.println("");
+        System.out.println("y el valor minimo es: "+ minimo);
+        JOptionPane.showMessageDialog(null, "El valor minimo del arreglo es:" + minimo, "Respuesta", JOptionPane.INFORMATION_MESSAGE);
+    }
+
 }
