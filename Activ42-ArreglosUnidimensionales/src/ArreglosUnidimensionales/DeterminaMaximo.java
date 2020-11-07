56504f09f6869fd2373d774d4ef8613ee644f9b3
cesar <cesar@DESKTOP-618VJAQ>
2020-11-07 14:48:45 -0600

    Determina maximo(Act 4.2)

--- Activ42-ArreglosUnidimensionales/src/ArreglosUnidimensionales/DeterminaMaximo.java
+++ Activ42-ArreglosUnidimensionales/src/ArreglosUnidimensionales/DeterminaMaximo.java
@@ -1,21 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ArreglosUnidimensionales;
 
-/**
- *
- * @author Darksempaii
- */
+import javax.swing.JOptionPane;
+
+
 public class DeterminaMaximo {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        int cantidad = Integer.parseInt(JOptionPane.showInputDialog(null, "Cuantos numeros van a ingresas", "Cantidad", JOptionPane.QUESTION_MESSAGE));
+        int[] numeros = new int[cantidad];
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingresa tu " + (i + 1) + "º dato", "Cantidad", JOptionPane.QUESTION_MESSAGE));
+        }
+        int maximo = 0;
+        System.out.print("Los numeros del arreglo son: ");
+        for (int i = 0; i < numeros.length; i++) {
+            if (maximo < numeros[i]) {
+                maximo = numeros[i];
+            }
+            System.out.print(numeros[i]+", ");
+        }
+        System.out.println("");
+        System.out.println("y el valor maximo es: "+ maximo);
+        JOptionPane.showMessageDialog(null, "El valor maximo del arreglo es:" + maximo, "Respuesta", JOptionPane.INFORMATION_MESSAGE);
     }
     
 }
