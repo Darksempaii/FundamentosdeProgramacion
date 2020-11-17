dd1872b236678b7ee84c7608b73dbfb23fec719c
cesar <cesar@DESKTOP-618VJAQ>
2020-11-17 15:30:22 -0600

    Mostrar Matriz En Consola(Act 4.4)

--- Activ45-ArreglosBiidimensionales/src/activ45/arreglosbiidimensionales/MuestraMatrizEnConsola.java
+++ Activ45-ArreglosBiidimensionales/src/activ45/arreglosbiidimensionales/MuestraMatrizEnConsola.java
@@ -1,21 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package activ45.arreglosbiidimensionales;
 
-/**
- *
- * @author cesar
- */
+import javax.swing.JOptionPane;
+
+
 public class MuestraMatrizEnConsola {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        int columna = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingresa el numero de columnas a usar", "Matriz", JOptionPane.QUESTION_MESSAGE));
+        int fila = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingresas el numero de filas a usar", "Matriz", JOptionPane.QUESTION_MESSAGE));
+        //Introduccion de datos
+        int[][] numeros = new int[fila][columna];
+        for (int i = 0; i < columna; i++) {
+            for (int j = 0; j < fila; j++) {
+                numeros[i][j] = Integer.parseInt(JOptionPane.showInputDialog(null, "Engresa un numero en la columno " + (i + 1) + " de la fila " + (j + 1), "Cantidad", JOptionPane.QUESTION_MESSAGE));
+            }
+        }
+        //Salida a consola 
+        System.out.println("Matriz");
+        for (int i = 0; i < fila; i++) {
+            for (int j = 0; j < columna; j++) {
+                System.out.print(numeros[i][j] + "\t");
+            }
+            System.out.println("");
+        }
     }
     
 }
