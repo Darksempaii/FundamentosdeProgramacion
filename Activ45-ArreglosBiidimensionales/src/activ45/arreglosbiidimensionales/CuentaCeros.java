58d86d48edb3c6dde7dd2cb121a14db71cf2cc4c
cesar <cesar@DESKTOP-618VJAQ>
2020-11-18 15:39:47 -0600

    correcion de errores menores

--- Activ45-ArreglosBiidimensionales/src/activ45/arreglosbiidimensionales/CuentaCeros.java
+++ Activ45-ArreglosBiidimensionales/src/activ45/arreglosbiidimensionales/CuentaCeros.java
@@ -12,9 +12,9 @@ public class CuentaCeros {
         int fila = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingresas el numero de filas a usar", "Matriz", JOptionPane.QUESTION_MESSAGE));
         //Introduccion de datos
         int[][] numeros = new int[fila][columna];
-        for (int i = 0; i < columna; i++) {
-            for (int j = 0; j < fila; j++) {
-                numeros[i][j] = Integer.parseInt(JOptionPane.showInputDialog(null, "Engresa un numero en la columno " + (i + 1) + " de la fila " + (j + 1), "Cantidad", JOptionPane.QUESTION_MESSAGE));
+        for (int i = 0; i < fila; i++) {
+            for (int j = 0; j < columna; j++) {
+                numeros[i][j] = Integer.parseInt(JOptionPane.showInputDialog(null, "Engresa un numero en la fila " + (i + 1) + " de la columna " + (j + 1), "Cantidad", JOptionPane.QUESTION_MESSAGE));
             }
         }
         //conversor de int a String y Contador de ceros
--- Activ45-ArreglosBiidimensionales/src/activ45/arreglosbiidimensionales/SumaPares.java
+++ Activ45-ArreglosBiidimensionales/src/activ45/arreglosbiidimensionales/SumaPares.java
@@ -1,21 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package activ45.arreglosbiidimensionales;
 
-/**
- *
- * @author cesar
- */
+import javax.swing.JOptionPane;
+
+
 public class SumaPares {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Declaracion del tamaño del arreglo
+        int columna = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingresa el numero de columnas a usar", "Matriz", JOptionPane.QUESTION_MESSAGE));
+        int fila = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingresas el numero de filas a usar", "Matriz", JOptionPane.QUESTION_MESSAGE));
+        //Introduccion de datos
+        int[][] numeros = new int[fila][columna];
+        for (int i = 0; i < fila; i++) {
+            for (int j = 0; j < columna; j++) {
+                numeros[i][j] = Integer.parseInt(JOptionPane.showInputDialog(null, "Engresa un numero en la fila " + (i + 1) + " de la columna " + (j + 1), "Cantidad", JOptionPane.QUESTION_MESSAGE));
+            }
+        }
+        //Suma de la matriz
+        int suma = 0;
+        for (int i = 0; i < fila; i++) {
+            for (int j = 0; j < columna; j++) {
+                suma = suma+numeros[i][j];
+            }
+        }
+        //Salida a consola 
+        System.out.println("Matriz");
+        for (int i = 0; i < fila; i++) {
+            for (int j = 0; j < columna; j++) {
+                System.out.print(numeros[i][j] + "\t");
+            }
+            System.out.println("");
+        }
+        System.out.println("El resultado de la suma de la matriz es: "+suma);
+        //Salida Grafica
+        JOptionPane.showMessageDialog(null, "El resultado de la suma es: " + suma, "Resultado", JOptionPane.INFORMATION_MESSAGE);
     }
     
 }
