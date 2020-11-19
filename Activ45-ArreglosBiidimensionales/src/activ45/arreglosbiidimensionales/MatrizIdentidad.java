e9c107367b17bffc32cbcf02283b80cfcd584171
cesar <cesar@DESKTOP-618VJAQ>
2020-11-18 18:52:37 -0600

    Matriz Identidad

--- Activ45-ArreglosBiidimensionales/src/activ45/arreglosbiidimensionales/MatrizIdentidad.java
+++ Activ45-ArreglosBiidimensionales/src/activ45/arreglosbiidimensionales/MatrizIdentidad.java
@@ -1,21 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package activ45.arreglosbiidimensionales;
 
-/**
- *
- * @author cesar
- */
+
 public class MatrizIdentidad {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+
+    public static void main(String[] args) throws InterruptedException {
+        //Introduccion de datos
+        int[][] numeros = new int[5][5];
+        //llenado de la matriz
+        for (int i = 0; i < numeros.length; i++) {
+            for (int j = 0; j < numeros[i].length; j++) {
+                if (i == j) {
+                    numeros[i][j]=1;
+                }else{
+                    numeros[i][j]=0;
+                }
+            }
+        }
+        //salida a consola
+        System.out.println("");
+        for (int i = 0; i < numeros.length; i++) {
+            for (int j = 0; j < numeros[i].length; j++) {
+                System.out.print(numeros[i][j]);
+                Thread.sleep(150);
+            }
+            System.out.println("");
+        }
+        System.out.println("");
     }
     
 }
