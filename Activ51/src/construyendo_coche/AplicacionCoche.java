580936136fe40154599d816107235cb53db1e56b
cesar <cesar@DESKTOP-618VJAQ>
2020-11-25 10:04:55 -0600

    actividad 5.1

---
+++ Activ51/src/construyendo_coche/AplicacionCoche.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package construyendo_coche;
+
+/**
+ *
+ * @author cesar
+ */
+public class AplicacionCoche {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
