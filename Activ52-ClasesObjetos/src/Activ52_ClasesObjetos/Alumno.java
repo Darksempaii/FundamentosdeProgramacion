75bd8eb5261aff4f468737ebc1e6aabc1de3c489
cesar <cesar@DESKTOP-618VJAQ>
2020-11-27 00:40:02 -0600

    actividad 5.1

--- Activ52-ClasesObjetos/src/Activ52_ClasesObjetos/Alumno.java
+++ Activ52-ClasesObjetos/src/Activ52_ClasesObjetos/Alumno.java
@@ -1,19 +1,19 @@
 package Activ52_ClasesObjetos;
 
 public class Alumno {
-
+    //declaracion de variables
     private String nombre;
     private String apellido;
     private int[] calif = new int[6];
 
-    public Alumno(String nom, String ape, int[] cal) {
+    public Alumno(String nom, String ape, int[] cal) { //constructor
         this.nombre = nom;
         this.apellido = ape;
         this.calif = cal;
     }
 
     @Override
-    public String toString() {
+    public String toString() { //metodo que regresa las variables
         String cadena = "";
         for (int i = 0; i < 10; i++) {
             cadena = cadena + calif[i] + "";
@@ -21,19 +21,17 @@ public class Alumno {
         return "\nNombre: " + nombre + "\nApellido: " + apellido + "\nCalificaciones: " + cadena;
     }
 
-    public int calcularMinimo() {
+    public int calcularMinimo() { //calcula la calificacion minima
         int minCalif = calif[0];
         for (int i = 0; i < calif.length; i++) {
             if (minCalif > calif[i]) {
                 minCalif = calif[i];
             }
         }
-            return minCalif;
-        }
+        return minCalif;
+    }
 
-    
-
-    public int calcularMaximo() {
+    public int calcularMaximo() { //calcula la calificacion maxima
         int maxCalif = calif[0];
         for (int i = 0; i < calif.length; i++) {
             if (maxCalif > calif[i]) {
@@ -43,7 +41,7 @@ public class Alumno {
         return maxCalif;
     }
 
-    public String tipoAlumno() {
+    public String tipoAlumno() { //calcula el tipo de alumno
         String tipo = "";
 
         return tipo;
--- Activ52-ClasesObjetos/src/Activ52_ClasesObjetos/PruebaAlumno.java
+++ Activ52-ClasesObjetos/src/Activ52_ClasesObjetos/PruebaAlumno.java
@@ -5,10 +5,12 @@ package Activ52_ClasesObjetos;
 public class PruebaAlumno {
 
     public static void main(String[] args) {
+        //declaracion de parametros
         int[] calificaciones = {7,8,9,9,7,9};
         int[] calificaciones2 = {8,8,7,7,8,10};
         Alumno jose = new Alumno("jose","aguirre",calificaciones);
         Alumno josefina = new Alumno("josefina","Gutierres",calificaciones2);
+        //salida a consola
         System.out.println(jose);
         System.out.println("Calif min: "+jose.calcularMinimo());
         System.out.println("Calif max: "+jose.calcularMaximo());
