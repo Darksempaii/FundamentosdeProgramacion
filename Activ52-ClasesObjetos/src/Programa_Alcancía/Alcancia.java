89de19bf672ae972edf6551c96510f8170ba4946
darksempaii <darksempaii@10.0.0.6>
2020-11-30 12:29:39 -0600

    programa Alcancia v2

--- Activ52-ClasesObjetos/src/Programa_Alcancía/Alcancia.java
+++ Activ52-ClasesObjetos/src/Programa_Alcancía/Alcancia.java
@@ -4,81 +4,72 @@ public class Alcancia {
 
     //Variables de la clase
     private String Nombre;
-    private int monto = 0;
     private int[] billetes = {0, 0, 0, 0, 0};
-    private int noAhorrado = 0;
     private int[] cantidades = {20, 50, 100, 200, 500};
 
     //constructor de la clase Alcancia
-    public Alcancia(String nom,int monto) {
+    public Alcancia(String nom) {
         this.Nombre = nom;
-        this.monto = monto;
 
     }
+
     @Override
-    public String toString(){
-    
-        return "El ahorro total de "+Nombre+" es de: "+calcualarTotalAhorrado()+"\nDinero ahorrado que no esta en la alcancia "+noAhorrado+"\ny el total de billetes es de \n"+ mostrarMonedas();
+    public String toString() {
+
+        return "El ahorro total de " + Nombre + " es de: " + calcualarTotalAhorrado() + "\ny el total de billetes es de \n" + mostrarMonedas();
     }
+
     //metodo ahorrar
-    private void Ahorrar() {
-        boolean Bucle = true;
+    public void ahorrar(int monto) {
         int Monto = monto;
-        do {
-            if (Monto >= 20 && Monto <= 49) {
+        switch (Monto) {
+            case 20:
                 billetes[0]++;
-                Monto = Monto - 20;
-            } else if (Monto >= 50 && Monto <= 100) {
+                break;
+            case 50:
                 billetes[1]++;
-                Monto = Monto - 50;
-            } else if (Monto >= 100 && Monto <= 199) {
+                break;
+            case 100:
                 billetes[2]++;
-                Monto = Monto - 100;
-            } else if (Monto >= 200 && Monto <= 499) {
+                break;
+            case 200:
                 billetes[3]++;
-                Monto = Monto - 200;
-            } else if (Monto >= 500) {
+                break;
+            case 500:
                 billetes[4]++;
-                Monto = Monto - 500;
-            } else {
-                noAhorrado = noAhorrado + Monto;
-                Monto = 0;
-                Bucle = false;
-            }
-        } while (Bucle);
-        monto = Monto;
-    }
-    //metodo Mostrar ahorros indexados
-    public String mostrarMonedas() {
-        String mostrar = "";
-        if (monto != 0) {
-            Ahorrar();
+                break;
+            default:
+                break;
         }
+    }
+
+    //metodo Mostrar ahorros indexados
+    private String mostrarMonedas() {
+        String mostrar = "";
         for (int i = 0; i < billetes.length; i++) {
             mostrar = mostrar + (cantidades[i] + "\t" + billetes[i] + "\n");
         }
         return mostrar;
     }
+
     //Metodo Calculo del total del ahorro
-    public int calcualarTotalAhorrado() {
-        Ahorrar();
+    private int calcualarTotalAhorrado() {
         int ahorro = 0;
         for (int i = 0; i < cantidades.length; i++) {
             for (int j = 1; j <= billetes[i]; j++) {
-                    ahorro = ahorro + cantidades[i];
+                ahorro = ahorro + cantidades[i];
             }
         }
-        ahorro = ahorro + noAhorrado;
         return ahorro;
     }
+
     //Metodo borrar ahorro
     public String romperAlcancia() {
         for (int i = 0; i < billetes.length; i++) {
             billetes[i] = 0;
         }
-        monto = 0;
-        noAhorrado = 0;
-        return Nombre+" haz roto la alcancia\n"+"Tu Ahorro es de "+calcualarTotalAhorrado()+"\nDinero ahorrado que no esta en la alcancia "+noAhorrado+"\ny tu total de billetes es de \n"+ mostrarMonedas();
+        //noAhorrado = 0;
+        return Nombre + " haz roto la alcancia\n" + "Tu Ahorro es de " + calcualarTotalAhorrado() + "\ny tu total de billetes es de \n" + mostrarMonedas();
     }
 
 }
--- Activ52-ClasesObjetos/src/Programa_Alcancía/AplicacionAlcancia.java
+++ Activ52-ClasesObjetos/src/Programa_Alcancía/AplicacionAlcancia.java
@@ -2,18 +2,33 @@ package Programa_Alcancía;
 
 public class AplicacionAlcancia {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         //declaracion de Objetos
-        Alcancia alcanciaDeJose = new Alcancia("Jose",450);
-        Alcancia alcanciaDeMaria = new Alcancia("Maria",380);
+        Alcancia alcanciaDeJose = new Alcancia("Jose");
+        Alcancia alcanciaDeMaria = new Alcancia("Maria");
+        //Ingreso de ahorros de jose
+        alcanciaDeJose.ahorrar(200);
+        alcanciaDeJose.ahorrar(200);
+        alcanciaDeJose.ahorrar(50);
+        //Ingreso de ahorros de maria
+        alcanciaDeMaria.ahorrar(200);
+        alcanciaDeMaria.ahorrar(100);
+        alcanciaDeMaria.ahorrar(20);
+        alcanciaDeMaria.ahorrar(20);
+        alcanciaDeMaria.ahorrar(20);
+        alcanciaDeMaria.ahorrar(20);
         //muestra de ahorros
         System.out.println(alcanciaDeJose);
+        Thread.sleep(500);
         System.out.println(alcanciaDeMaria);
+        Thread.sleep(500);
         //Vaciado de alcancias
         System.out.println(alcanciaDeJose.romperAlcancia());
+        Thread.sleep(500);
         System.out.println(alcanciaDeMaria.romperAlcancia());
-
-        
+        Thread.sleep(500);
+        //Fin del programa
+        System.exit(0);
     }
 
 }
